c0a16270af0ec094ec8e1f6642d25cd841a56fa5
john <jmlovitsch@gmail.com>
2020-11-01 15:55:27 -0600

    commit tree aa93aa0f7dd41f58ad26e5c1a49d71f130b925eb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    const mDr = drivers.filter(d => d.toUpperCase() === string.toUpperCase() )
+    return mDr
+}
+
+function fuzzyMatch(drivers, string) {
+    const mDr = drivers.filter(d => d.slice(0, string.length ) === string )
+    return mDr
+}
+
+function matchName(drivers, string) {
+    const mDr = drivers.filter(d => d.name.toUpperCase() === string.toUpperCase() )
+    return mDr
+
+}
